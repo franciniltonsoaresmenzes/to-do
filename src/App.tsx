8491f2fb73f1cf6b3d6256895acd67fbe22e5b4d
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-11 18:42:58 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './global.css'
+import { Header } from './components/Header'
+import { AddNewTask } from './components/AddNewTask'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <AddNewTask/>
+    </>
+  )
+}
+
+export default App

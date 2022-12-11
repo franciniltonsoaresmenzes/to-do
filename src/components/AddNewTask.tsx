836121513fffaa732a3baaa238361dea0c8e1293
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-11 18:42:53 -0300

    adicionando components Add new task

---
+++ src/components/AddNewTask.tsx
@@ -0,0 +1,10 @@
+import styles from './AddNewTask.module.css'
+
+export function AddNewTask() {
+  return (
+    <form>
+      <input type="text"/>
+      <button type='button' >Criar</button>
+    </form>
+  )
+}

daf1471ca02b579b79c02f94ca6fd8cd32902034
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-11 18:42:22 -0300

    adicionando header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+import logoRocket from '../assets/rocket.svg'
+
+export function Header() {
+  return (
+    <header>
+      <div className={styles.logo} >
+        <img src={logoRocket} alt="Logo" />
+        <h1>to<span>do</span></h1>
+      </div>
+    </header>
+  )
+}

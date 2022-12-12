be62e948e5cee38c25edfadcf38b6b516e606618
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-12-11 21:14:46 -0300

    finalizado compenente Collection Task

--- src/components/CollectionTask.tsx
+++ src/components/CollectionTask.tsx
@@ -1,14 +1,28 @@
 import { Check, Trash } from 'phosphor-react'
+import { useState } from 'react'
 import styles from './CollectionTask.module.css'
 
 export function CollectionTask() {
+  const [icon, setIcon] = useState(false)
+
+  function handleCheck() {
+    setIcon(!icon)
+  }
+
   return(
     <div className={styles.card} >
-      <button type='button' >
-        <Check/>
+      <button 
+        type='button'
+        className={icon ? styles.true : styles.false}
+        onClick={handleCheck}
+      >
+        { icon ? <Check weight='bold' />  : null}
       </button>
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-      <Trash size={24} />
+      <p className={icon ? styles.checked : styles.noChecked} >Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <Trash 
+        size={12}
+        className={styles.trash}
+      />
     </div>
   )
 }
